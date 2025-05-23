bfeb28920e71d285e43a2fa7077c7182dbc1ddfb
Aamer Shahzad <a.shahzad@digifloat.com>
2025-05-23 16:20:26 +0500

    Initial commit

---
+++ .storybook/preview.ts
@@ -0,0 +1,23 @@
+
+import type { Preview } from '@storybook/react';
+
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+import '../src/index.css';
+
+// Registers the msw addon
+initialize();
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  loaders: [mswLoader],
+};
+
+export default preview;
